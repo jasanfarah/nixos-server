2f1fea55cb96c5e26b80e02020190f1d6576a91c
Jasan <mail@farah.dk>
2025-05-13 11:11:47 -0400

    pinned nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,13 +1,14 @@
 {
-  description = "A very basic flake";
-
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.11"; };
 
   outputs = { self, nixpkgs }: {
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+    nixosConfigurations = {
+      server = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./server/configuration.nix ];
+      };
+    };
   };
 }
+
